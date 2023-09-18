626bc1a450707ee5229b2da116d455c223dc1d2a
Volkamr <juan.volkmar@upb.edu.co>
2023-09-17 19:10:40 -0500

    server

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +27,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>{!data ? "Loading..." : data}</p>
     </div>
   );
 }
